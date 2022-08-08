3f23dc9f25d48ce3aa672c3cbbbea45451f56cbd
OGDee93 <a@b.c>
2022-08-08 17:07:27 +0100

    initial commit

---
+++ src/bo/buypy.py
@@ -0,0 +1,8 @@
+"""
+BuyPy
+A command-line backoffice application. This is an interactive 
+shell application
+
+(c) Ana Mendes e Diogo Ferreira, 2022
+"""
+
---
+++ src/db/buypy.sql
@@ -0,0 +1,13 @@
+/*
+    DDL for the BuyPy Online Store.
+    (c) Ana Mendes e Diogo Ferreira
+*/
+
+DELIMITER //
+
+DROP DATABASE IF NOT EXISTS BuyPy
+//
+CREATE DATABASE BuyPy
+//
+
+USE BuyPy
